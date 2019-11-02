a05405a4edf3deb74ed467100225d11d08a7e4e6
Tomás Francisco <mail@tomasfrancisco.com>
2019-11-02 20:37:27 +0100

    add support to emotion

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,3 +1,18 @@
+import { css } from "@emotion/core";
 import React, { FC } from "react";
 
-export const Button: FC = ({ ...props }) => <button {...props} />;
+export const Button: FC = ({ ...props }) => (
+  <button
+    css={css`
+      padding: 10px 20px;
+      border: 1px solid #efefef;
+      text-transform: uppercase;
+      cursor: pointer;
+
+      &:hover {
+        border-color: #3f3f3f;
+      }
+    `}
+    {...props}
+  />
+);
